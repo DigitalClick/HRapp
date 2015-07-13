786d051b03688175e5a1e62bb4666764f9fc2d9c
DigitalClick <mathews.antony@yahoo.com>
2015-07-13 17:30:42 +0530

    hr App 1.2

---
+++ bld/Debug/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Debug\platforms\android\ant-build\AndroidManifest.xml : \
+C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Debug\platforms\android\AndroidManifest.xml \
