c9c3a814a2c42b18975c2a8d5b108e0c94517d72
DigitalClick <mathews.antony@yahoo.com>
2015-07-13 16:06:58 +0530

    Revert "Hr App V1.0"
    This reverts commit c7c7a913dd12c371d288fe93a75b25c46b1cac0a.

--- bin/Android/Release/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Release\platforms\android\ant-build\AndroidManifest.xml : \
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Release\platforms\android\AndroidManifest.xml \
--- bin/Android/Release/CordovaApp-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Release\platforms\android\ant-build\CordovaApp-release-unsigned.apk : \
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Release\platforms\android\ant-build\CordovaApp.ap_ \
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Release\platforms\android\ant-build\classes.dex \
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Release\platforms\android\CordovaLib\ant-build\classes.jar \
