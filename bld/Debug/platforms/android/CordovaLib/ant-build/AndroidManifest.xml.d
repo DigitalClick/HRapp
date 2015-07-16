6760925a3be2f03779ed6525baa5bedc62efad79
DigitalClick <mathews.antony@yahoo.com>
2015-07-16 15:56:31 +0530

    HR App 2.0
    HR mobile

--- bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\MyData\Kareem\CMTApp\MobApp1\MobApp1\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
